84509cd4fdd2cb7da456efae89176dbe10cdb68a
Subhomoy Pal <subhomoypal@Subhomoys-MacBook-Pro.local>
2023-03-19 21:47:54 +0530

    create home component and add notes about project

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home(props) {
+  return (
+    <>
+      <section className="home-wrapper">
+        <div className="container"></div>
+      </section>
+    </>
+  );
+}
+
+export default Home;
